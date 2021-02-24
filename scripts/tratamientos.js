e703d7529c62c0fc307b1e5c8ed3a4e9f95c9680
ulysses316 <ulises316@live.com.mx>
2021-02-24 00:21:31 -0600

    Correcion efecto dropdown

--- scripts/tratamientos.js
+++ scripts/tratamientos.js
@@ -5,11 +5,11 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
+    if (content.style.display == "none"){
+      content.style.display = "block";
     } 
     else {
-      content.style.maxHeight = content.scrollHeight + "0px";
+      content.style.display = "none";
     }
   });
 }
